b0004c7d4729674b2e7090cd8dddcd2b981b589a
Saipavan1612 <saipavangurram@gmail.com>
2024-08-30 12:24:38 +0530

    minor changes

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard-item',
+    standalone: true,
+    imports: [],
+    templateUrl: './dashboard-item.component.html',
+    styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+    @Input({ required: true }) image!: { src: string, alt: string };
+    // ! => It tells angular to image property won't be undefined in any case
+
+    @Input({ required: true }) title!: string;
+
+    //image = input.required<{src: string, alt: string}>(); USING INPUT SIGNAL
+    //title = input.required<string>(); USING INPUT SIGNAL
+}
